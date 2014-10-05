f263bd73cdf547bb403e310fb92212ebfd9c2fa8
Alex Wendland <me@alexwendland.com>
2014-10-04 18:46:07 -0700

    New image build demos. Pretty flower demo and incomplete president demo, both using web workers

---
+++ example/president/worker.js
@@ -0,0 +1,14 @@
+importScripts('http://rawgithub.com/awendland/filling-algorithms-js/master/src/rnd-fill.js');
+onmessage = function (oEvent) {
+	var points = [],
+		width = oEvent.data.width,
+		height = oEvent.data.height,
+		pixelSize = oEvent.data.pixelSize;
+	while(true) {
+		var result = RndFill.bestOf(points, 100, width, height);
+		if (points.length === 0 || result.dist > pixelSize - 1) {
+			points.push(result.best);
+			postMessage(result.best);
+		}
+	}
+};
