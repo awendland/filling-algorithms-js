7edb7eabaac40b4e630aa01832643a7a31e95301
Alex Wendland <me@alexwendland.com>
2014-10-04 18:57:50 -0700

    Full page fill implemented with a Web Worker

---
+++ example/full-page-worker/worker.js
@@ -0,0 +1,11 @@
+importScripts('http://rawgithub.com/awendland/filling-algorithms-js/master/src/rnd-fill.js');
+onmessage = function (oEvent) {
+	var points = [],
+		width = oEvent.data.width,
+		height = oEvent.data.height;
+	while(true) {
+		var result = RndFill.bestOf(points, 10, width, height);
+		points.push(result.best);
+		postMessage(result);
+	}
+};
